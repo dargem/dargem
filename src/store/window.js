471aa059e7939bd71be2fc3614069d545be8579c
dargem <genericz145@gmail.com>
2025-12-20 03:11:58 +1100

    windowjs

---
+++ src/store/window.js
@@ -0,0 +1,31 @@
+import { INITIAL_Z_INDEX } from "#";
+import { WINDOW_CONFIG } from "#";
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+
+const useWindowStore = create(
+    immer((set) => ({
+        windows: WINDOW_CONFIG,
+        nextZIndex: INITIAL_Z_INDEX + 1,
+
+        openWindow: (windowKey, data=null) => set((state => {
+            const win = state.windows[windowKey];
+            win.isOpen = true;
+            win.zIndex = state.nextZIndex;
+            win.data = data ?? win.data;
+            state.nextZIndex++;
+        })),
+
+        closeWindow: (windowKey) => set((state => {
+            const win = state.windows[windowKey];
+            win.isOpen = false;
+            win.zIndex = INITIAL_Z_INDEX;
+            win.data = null;
+        })),
+
+        focusWindow: (windowKey) => set((state => {
+            const win = state.windows[windowKey];
+            win.isOpen = false;
+            win.zIndex = state.nextZIndex++;
+        })),
+})));
