c0a86408ac19827874e96e323aca47d8c9b0cceb
dargem <genericz145@gmail.com>
2025-12-19 23:46:53 +1100

    better nav stuff

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
-import { navLinks } from "#constants/index.js"
+import dayjs from "dayjs";
+
+import { navLinks, navIcons } from "#constants";
 
 const NavBar = () => {
     return (
-        <nav>
+        <nav className="mt-2 ml-2 mr-2">
             <div>
-                <img src="/files/images/endeavour_logo.svg" alt="logo" width="50px" height="50px"/>
-                <p class="font-bold">My Portfolio</p>
+                <img src="/files/images/endeavour_logo.svg" alt="logo" className="w-6 h-6"/>
+                <p className="font-bold">My Portfolio</p>
 
                 <ul>
                     {navLinks.map(({ id, name }) => (
@@ -14,6 +16,21 @@ const NavBar = () => {
                         </li>
                     ))}
                 </ul>
+            </div>
+
+            <div>
+                <ul className="flex items-center gap-5 list-none">
+                    {navIcons.map(({ id, img }) => (
+                        <li key={id}>
+                            <img 
+                                src={img} 
+                                className="icon-hover icon w-6 h-6" 
+                                alt={`icon-${id}`}
+                            />
+                        </li>
+                    ))}
+                </ul>
+                <time>{dayjs().format('ddd MMM D h:mm A')}</time>
             </div>
         </nav>
     )
