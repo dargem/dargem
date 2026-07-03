c47f76a616bac8209ba7d2ae5dbe450ce36d50eb
dargem <genericz145@gmail.com>
2026-07-03 17:51:26 +1000

    pray vercel fixes itself

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
     return (
         <nav>
             <div>
-                <img src="/files/images/arch_logo.png" alt="logo" className="h-6"/>
+                <img src="/files/images/portfolio.png" alt="logo" className="h-6"/>
                 <p className="font-bold">My Portfolio</p>
                 <ul>
                     {navLinks.map(({ id, name, type }) => (
